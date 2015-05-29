57038d97c85142a334d197fb806bc77fda149c22
Parker Holladay <parker.holladay@gmail.com>
2015-05-28 21:17:59 -0600

    Evaluate for 3 and 4

--- FizzBuzz.Tests/FizzBuzzCalculatorTests.cs
+++ FizzBuzz.Tests/FizzBuzzCalculatorTests.cs
@@ -7,11 +7,20 @@ namespace FizzBuzz.Tests
     {
         [TestCase(1)]
         [TestCase(2)]
+        [TestCase(4)]
         public void CalculatorShouldReturnNumberAsString(int input)
         {
             var calculator = new FizzBuzzCalculator();
 
             Assert.That(calculator.Calculate(input), Is.EqualTo(input.ToString()));
+        }
+
+        [Test]
+        public void CalculatorShouldReturnFizz()
+        {
+            var calculator = new FizzBuzzCalculator();
+
+            Assert.That(calculator.Calculate(3), Is.EqualTo("Fizz"));
         }
     }
 }
--- FizzBuzz/FizzBuzzCalculator.cs
+++ FizzBuzz/FizzBuzzCalculator.cs
@@ -4,6 +4,7 @@
     {
         public string Calculate(int input)
         {
+            if (input == 3) return "Fizz";
             return input.ToString();
         }
     }
