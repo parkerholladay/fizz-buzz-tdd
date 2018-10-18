3511df4c670ccce199a63dc7f4399ad7535a6d1d
Parker Holladay <parker-holladay@pluralsight.com>
2018-10-17 22:19:38 -0600

    Evaluate 7 to `Bizz`

--- FizzBuzz.Tests/FizzBuzzCalculatorTests.cs
+++ FizzBuzz.Tests/FizzBuzzCalculatorTests.cs
@@ -15,22 +15,24 @@ namespace FizzBuzz.Tests
                 yield return new TestCaseData(4).Returns("4");
                 yield return new TestCaseData(5).Returns("Buzz");
                 yield return new TestCaseData(6).Returns("Fizz");
-                yield return new TestCaseData(7).Returns("7");
+                yield return new TestCaseData(7).Returns("Bizz");
                 yield return new TestCaseData(8).Returns("8");
                 yield return new TestCaseData(9).Returns("Fizz");
                 yield return new TestCaseData(10).Returns("Buzz");
                 yield return new TestCaseData(11).Returns("11");
                 yield return new TestCaseData(12).Returns("Fizz");
                 yield return new TestCaseData(13).Returns("13");
-                yield return new TestCaseData(14).Returns("14");
+                yield return new TestCaseData(14).Returns("Bizz");
                 yield return new TestCaseData(15).Returns("FizzBuzz");
                 yield return new TestCaseData(16).Returns("16");
                 yield return new TestCaseData(17).Returns("17");
                 yield return new TestCaseData(18).Returns("Fizz");
                 yield return new TestCaseData(19).Returns("19");
                 yield return new TestCaseData(20).Returns("Buzz");
+                yield return new TestCaseData(21).Returns("FizzBizz");
                 yield return new TestCaseData(25).Returns("Buzz");
                 yield return new TestCaseData(30).Returns("FizzBuzz");
+                yield return new TestCaseData(35).Returns("BuzzBizz");
                 yield return new TestCaseData(45).Returns("FizzBuzz");
             }
         }
--- FizzBuzz/FizzBuzzCalculator.cs
+++ FizzBuzz/FizzBuzzCalculator.cs
@@ -4,9 +4,13 @@
     {
         public string Calculate(int input)
         {
-            return input % 15 == 0 ? "FizzBuzz" :
+            return
+                input % 35 == 0 ? "BuzzBizz" :
+                input % 21 == 0 ? "FizzBizz" :
+                input % 15 == 0 ? "FizzBuzz" :
                 input % 3 == 0 ? "Fizz" :
                 input % 5 == 0 ? "Buzz" :
+                input % 7 == 0 ? "Bizz" :
                 input.ToString();
         }
     }
