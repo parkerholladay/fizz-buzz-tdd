3412d76d3e8ec71870d80f1386a6b31f97620fa2
Parker Holladay <parker-holladay@pluralsight.com>
2018-10-17 21:32:12 -0600

    Convert to .net core 2.1

--- FizzBuzz/FizzBuzzCalculator.cs
+++ FizzBuzz/FizzBuzzCalculator.cs
@@ -4,9 +4,9 @@
     {
         public string Calculate(int input)
         {
-            return input%15 == 0 ? "FizzBuzz" :
-                input%3 == 0 ? "Fizz" :
-                input%5 == 0 ? "Buzz" :
+            return input % 15 == 0 ? "FizzBuzz" :
+                input % 3 == 0 ? "Fizz" :
+                input % 5 == 0 ? "Buzz" :
                 input.ToString();
         }
     }
